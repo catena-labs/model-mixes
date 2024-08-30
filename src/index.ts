4ee373f13b6ef88ba20854cc9b1271c010997d29
Matt Venables <matt@catena.xyz>
2024-08-29 20:42:59 -0400

    v0.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { lmsysCoding } from "./mixes/lmsys-coding"
 import { lmsysOverall } from "./mixes/lmsys-overall"
+import { moaCoding } from "./mixes/moa-coding"
+import { sealSpanish } from "./mixes/seal-spanish"
 
-export const mixes = {
+export const modelMixes = {
   lmsysCoding,
-  lmsysOverall
+  lmsysOverall,
+  moaCoding,
+  sealSpanish
 }
