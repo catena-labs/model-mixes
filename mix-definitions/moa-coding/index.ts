7f6e87391dae271cadd3bf0e3df5816cb79d6427
Dominic Bosco <dominic@catena.xyz>
2024-09-04 08:06:54 -0400

    Update mix docUrls

--- mix-definitions/moa-coding/index.ts
+++ mix-definitions/moa-coding/index.ts
@@ -1,3 +1,4 @@
+import { getDocsUrl } from "../../utils/docs-url"
 import type { MoaModelMixDefinition } from "../types"
 import readme from "./README.md"
 
@@ -10,8 +11,7 @@ export default {
   createdAt: new Date("2024-08-26T13:00:00-04:00"), // August 26, 2024 1pm Eastern
   description:
     "A mixture-of-agents that synthesizes coding answers from the best LLMs for state-of-the-art performance.",
-  docsUrl:
-    "https://github.com/catena-labs/model-mixes/tree/main/src/mixes/moa-coding",
+  docsUrl: getDocsUrl("moa-coding"),
   name: "MoA SynthCode Mix",
   readme,
   slug: "moa-coding",
