5dd527605ad6c477ab1af0bd52817ec1eec91e94
Dominic Bosco <dominic@catena.xyz>
2024-09-11 14:08:22 -0400

    v0.0.11 - export all mix definitions

--- mix-definitions/index.ts
+++ mix-definitions/index.ts
@@ -1,4 +1,7 @@
+import crosshatchChatOs from "./crosshatch-chat-os"
 import lmsysCoding from "./lmsys-coding"
+import lmsysCodingOs from "./lmsys-coding-os"
+import lmsysMultiturn from "./lmsys-multiturn"
 import lmsysOverall from "./lmsys-overall"
 import moaCoding from "./moa-coding"
 import sealSpanish from "./seal-spanish"
@@ -6,7 +9,10 @@ import type { ModelMixDefinition } from "./types"
 
 export * from "./types"
 export const modelMixes: Record<string, ModelMixDefinition> = {
+  crosshatchChatOs,
   lmsysCoding,
+  lmsysCodingOs,
+  lmsysMultiturn,
   lmsysOverall,
   moaCoding,
   sealSpanish
