3a0f49004a69d835e8ae37ac092bee9651002afc
Matt Venables <matt@catena.xyz>
2024-12-18 12:59:54 -0500

    Update mix description

--- mix-definitions/super-sonnet-mix/index.ts
+++ mix-definitions/super-sonnet-mix/index.ts
@@ -25,7 +25,7 @@ export default {
   },
   createdAt: new Date("2024-12-18T10:00:00-04:00"),
   description:
-    "The Super Sonnet Mix bundles multiple tools and providers in a single endpoint to supercharge Claude's Sonnet 3.5 model. Simply send a request with a Crosshatch tool (eg: crosshatch:create_image) to use it. Additionally, the Super Sonnet Mix frees you from rate-limit issues by consolidating several inference providers. If your request is rate limited by Anthropic, Crosshatch will automatically route it to Bedrock.",
+    "Super Sonnet Mix bundles multiple tools and providers in a single endpoint to supercharge Anthropic's Sonnet 3.5 model. Simply send a request with a Crosshatch tool (eg: crosshatch:create_image) to use it. Additionally, the Super Sonnet Mix frees you from rate-limit issues by consolidating several inference providers. If your request is rate limited by Anthropic, Crosshatch will automatically route it to Bedrock.",
   docsUrl: getDocsUrl("super-sonnet-3.5-20241022"),
   name: "Super Sonnet Mix",
   readme,
